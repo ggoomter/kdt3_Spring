00876a2fb0448b15c7db0a2ba16d37b111e33797
ggoomter <ggoomter@gmail.com>
2022-10-11 14:12:45 +0900

    logj4dbc 의존성 추가. 쿼리결과를 테이블의 형태로 보여줌

--- VamPa_Board(Oracle)/src/test/java/com/vam/persistence/DataSourceTest.java
+++ VamPa_Board(Oracle)/src/test/java/com/vam/persistence/DataSourceTest.java
@@ -1,7 +1,11 @@
 package com.vam.persistence;
 
 import java.sql.Connection;
+
 import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +19,20 @@ public class DataSourceTest {
     @Autowired
     private DataSource dataSource;
     
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+    
     @Test
     public void testConnection() {
     	System.out.println("이까지는 되는지 테스트");
-        try(Connection con = dataSource.getConnection();){
+        try(
+        		Connection con = dataSource.getConnection();
+        		SqlSession session = sqlSessionFactory.openSession();
+        	)
+        {
             
             System.out.println("con = " + con);
+            System.out.println("session = " + session);
             
         } catch(Exception e) {
             
