7af402f291c9c05a0d77cabb6a017906abf75834
ggoomter <ggoomter@gmail.com>
2022-10-12 11:35:02 +0900

    컨트롤러에 enroll화면에서 작성한 데이터 넘어오는것 확인

--- VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
+++ VamPa_Board(Oracle)/src/main/java/com/vam/controller/BoardController.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.vam.model.BoardVO;
 
 @Controller
 @RequestMapping("/board/*")
@@ -21,10 +24,17 @@ public class BoardController {
         log.info("게시판 목록 페이지 진입");
     }
     
+    /* 게시판 등록 화면 */
     @GetMapping("/enroll")
     // => @RequestMapping(value="enroll", method=RequestMethod.GET)
     public void boardEnrollGET() {
         log.info("게시판 등록 페이지 진입");
+    }
+    
+    /* 게시판 등록 기능 */
+    @PostMapping("/enroll")
+    public void boardEnrollPOST(BoardVO board) {
+        log.info("BoardVO : " + board);
     }
 	
 }
--- VamPa_Board(Oracle)/src/main/webapp/WEB-INF/views/board/enroll.jsp
+++ VamPa_Board(Oracle)/src/main/webapp/WEB-INF/views/board/enroll.jsp
@@ -8,5 +8,20 @@
 </head>
 <body>
 	<h1>게시판 등록</h1>
+	<form action="/board/enroll" method="post">
+	    <div class="input_wrap">
+	        <label>Title</label>
+	        <input name="title">
+	    </div>
+	    <div class="input_wrap">
+	        <label>Content</label>
+	        <textarea rows="3" name="content"></textarea>
+	    </div>
+	    <div class="input_wrap">
+	        <label>Writer</label>
+	        <input name="writer">
+	    </div>
+	    <button class="btn">등록</button>
+	</form>
 </body>
 </html>
